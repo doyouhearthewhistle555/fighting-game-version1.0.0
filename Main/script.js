2e8e81ee2dc37ce83b54429ee20a996977dcf9af
doyouhearthewhistle555 <coolgithubuser@gmail.com>
2025-07-27 17:05:49 +0800

    update script.js

--- Main/script.js
+++ Main/script.js
@@ -4,6 +4,8 @@ const c = document.getElementById("label1")
 const d = document.getElementById("label2")
 let blueteamHealth = 10000
 let redteamHealth = 10000
+let blueteamtapCount = 0
+let redteamtapCount = 0
 updateLabels()
 function updateLabels(){
   c.textContent = `${blueteamHealth} Health`
@@ -17,11 +19,16 @@ function resetGame(){
 function reduceredteamHealth(){
   redteamHealth = redteamHealth - 10
   updateLabels()
-  if(redteamHealth === 0){
+  if(redteamHealth < 0){
     alert("Blue Team Wins! the game will be reseted.")
     redteamHealth = 0
     updateLabels()
       resetGame()
+  }
+  blueteamtapCount = blueteamtapCount + 1
+  if(blueteamtapCount > 10){
+    reduceredteamHealth = reduceredteamHealth - 25
+    updateLabels()
   }
 }
 function gainblueteamHealth(){
@@ -35,7 +42,7 @@ function gainblueteamHealth(){
 function reduceblueteamHealth(){
   blueteamHealth = blueteamHealth - 10
   updateLabels()
-  if(blueteamHealth === 0){
+  if(blueteamHealth < 0){
     alert("Red Team Wins resetting game...")
     blueteamHealth = 0
     updateLabels()
@@ -50,7 +57,8 @@ function gainredteamHealth(){
     updateLabels()
   }
 }
-a.addEventListener('click', reduceredteamHealth)
-a.addEventListener('click', gainblueteamHealth)
-b.addEventListener('click', reduceblueteamHealth)
-b.addEventListener('click', gainredteamHealth)
+a.addEventListener('touchstart', reduceredteamHealth)
+a.addEventListener('touchstart', gainblueteamHealth)
+b.addEventListener('touchstart', reduceblueteamHealth)
+b.addEventListener('touchstart', gainredteamHealth)
+alert("The First button is at the top which is Blue Team, the second button at the bottom is red team, get one team to 0 health to win this is a game to test your tapping speed, you must have a friend or person to play with you, you can still play solo but HAVE FUN!")
